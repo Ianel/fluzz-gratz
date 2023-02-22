e6d233c6407e5f1868605dccc1210b2157cfaa87
Ianel Tombozafy <ianell.s118@gmail.com>
2023-02-22 22:02:19 +0300

    feat: implementing logic

--- src/App.java
+++ src/App.java
@@ -1,6 +1,18 @@
 
 public class App {
     public static void main(String[] args) throws Exception {
-        
+        int[] numbersToTest = new int[]{24, 45, 2, 8, 9, 10, 15};
+
+        for (int number: numbersToTest) {
+            if (number % 3 == 0 && number % 5 == 0) {
+                System.out.println("FluzzGratz");
+            } else if (number % 3 == 0) {
+                System.out.println("Fluzz");
+            } else if (number % 5 == 0) {
+                System.out.println("Gratz");
+            } else {
+                System.out.println(number);
+            }
+        }   
     }
 }
